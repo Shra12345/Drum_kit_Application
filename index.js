82be4da97c28a0fb4ee80650718a371afe977375
Shradha Jadhav <53149457+Shra12345@users.noreply.github.com>
2020-10-25 13:36:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+for (var i = 0; i< document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+  var sound = new Audio("sounds/tom-1.mp3");
+  sound.play();
+   //this.style.color = "white";
+    // alert("hello");
+  var buttoninnerhtml = this.innerHTML;
+  makeSound(buttoninnerhtml);
+
+
+}
+);
+}
+
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch(key)
+  {
+  case "w":
+   var tom1 = new Audio("sounds/tom-1.mp3");
+   tom1.play();
+   break;
+
+   case "a":
+    var tom2 = new Audio("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+
+   case "s":
+     var tom3 = new Audio("sounds/tom-3.mp3");
+     tom3.play();
+     break;
+
+   case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+  case "j":
+       var tom5 = new Audio("sounds/crash.mp3");
+       tom5.play();
+       break;
+
+
+  case "k":
+        var tom6 = new Audio("sounds/kick-bass.mp3");
+        tom6.play();
+        break;
+
+ case "l":
+
+    var tom1 = new Audio("sounds/snare.mp3");
+    tom1.play();
+    break;
+
+ default:console.log(buttoninnerhtml);
+
+}
+}
+/*function handleclick() {
+var sound = new Audio("sounds/tom-1.mp3");
+sound.play();
+}*/
